d6cc44ab3bb1185b1c841ab8609bf9353801268b
gintamamadao <gintamaginsan@126.com>
2019-10-11 03:30:06 +0800

    1.0-index

--- src/index.js
+++ src/index.js
@@ -1,2 +1,30 @@
 const Shipit = require("shipit-cli");
 const deploy = require("shipit-deploy");
+const npm = require("./npm");
+const copy = require("./copy");
+const pm2 = require("./pm2");
+const plugin = require("./plugin");
+const error = require("./error");
+
+async function projectDeploy(commenConf, envConf) {
+    const shipit = new Shipit({ environment: "special" });
+    const runTasks = ["deploy"];
+    const config = {
+        default: commenConf,
+        special: envConf
+    };
+
+    deploy(shipit);
+    npm(shipit);
+    copy(shipit);
+    pm2(shipit);
+    plugin(shipit);
+    error(shipit);
+
+    shipit.initConfig(config);
+    shipit.initialize();
+
+    shipit.start(runTasks);
+}
+
+module.exports = projectDeploy;
