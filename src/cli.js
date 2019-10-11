40cd97f54960fbc4bcdbd416a6b6718df8e43eac
gintamamadao <gintamaginsan@126.com>
2019-10-12 01:53:04 +0800

    1.0-cli

--- src/cli.js
+++ src/cli.js
@@ -6,7 +6,12 @@ const interpret = require("interpret");
 const v8flags = require("v8flags");
 const Liftoff = require("liftoff");
 const minimist = require("minimist");
+const path = require("path");
+const fs = require("fs");
 const program = new commander.Command();
+const { Type } = require("schema-verify");
+const projectDeploy = require("./index");
+const { ERR_MSG } = require("./constant");
 
 program
     .version("v1.0")
@@ -14,7 +19,8 @@ program
     .usage("<project> <env> <tag>")
     .option("--project <project>", "project name")
     .option("--env <env>", "environment")
-    .option("--tag <tag>", "git tag");
+    .option("--tag <tag>", "git tag")
+    .option("--filePath <filePath>", "config file path");
 
 const processArgv = process.argv.slice(2);
 if (processArgv.length <= 0) {
@@ -23,19 +29,55 @@ if (processArgv.length <= 0) {
 const argv = minimist(processArgv);
 
 const onExecute = function(liftEnv) {
+    const cwd = liftEnv.cwd;
     const project = argv.project;
     const env = argv.env;
     const tag = argv.tag;
+    const filePath = argv.filePath;
+    let defaultConf = {};
+    let envConf = {};
+    if (Type.string.isNotEmpty(project) && Type.string.isNotEmpty(env)) {
+        const defaultConfPath = path.join(cwd, project, `default.js`);
+        const envConfPath = path.join(cwd, project, `${env}.js`);
+        if (fs.existsSync(defaultConfPath)) {
+            defaultConf = require(defaultConfPath);
+        }
+        if (fs.existsSync(envConfPath)) {
+            envConf = require(envConfPath);
+        }
+    }
+    if (Type.string.isNotEmpty(filePath)) {
+        if (fs.existsSync(filePath)) {
+            envConf = require(filePath);
+        }
+    }
+    if (
+        !Type.object.isNotEmpty(defaultConf) &&
+        !Type.object.isNotEmpty(envConf)
+    ) {
+        throw new Error(ERR_MSG.emptyConfig);
+    }
+    projectDeploy(defaultConf, envConf, tag);
 };
 
 const onPrepare = function(liftEnv) {
     const project = argv.project;
     const env = argv.env;
     const tag = argv.tag;
+    const filePath = argv.filePath;
 
-    console.log(chalk.blue(`project: ${project}`));
-    console.log(chalk.blue(`environment: ${env}`));
-    console.log(chalk.blue(`tag: ${tag}`));
+    if (Type.string.is(project)) {
+        console.log(chalk.blue(`project: ${project}`));
+    }
+    if (Type.string.is(env)) {
+        console.log(chalk.blue(`project: ${env}`));
+    }
+    if (Type.string.is(tag)) {
+        console.log(chalk.blue(`project: ${tag}`));
+    }
+    if (Type.string.is(filePath)) {
+        console.log(chalk.blue(`project: ${filePath}`));
+    }
 
     cli.execute(liftEnv, onExecute);
 };
--- src/constant.js
+++ src/constant.js
@@ -10,7 +10,8 @@ const ERR_MSG = {
     needWorkspace: "缺少工作目录",
     errorCopyInfo: "错误的复制配置",
     errorPm2Info: "错误的PM2配置",
-    errorPluginInfo: "错误的插件配置"
+    errorPluginInfo: "错误的插件配置",
+    emptyConfig: "缺少部署配置"
 };
 
 module.exports = { EVENTS, ERR_MSG };
